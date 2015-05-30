9e23b52c1bef001fa5ee962f3fef942f74e3d183
John <lai.john.py@gmail.com>
2015-05-29 23:19:27 -0400

    added header styles and banner img

--- page-chef.php
+++ page-chef.php
@@ -1,27 +1,81 @@
-<?php get_header();  ?>
+
+
+
+
+<?php //index.php is the last resort template, if no other templates match ?>
+<?php get_header(); ?>
 
 <div class="main">
   <div class="container">
 
-  <p>Editing page.php</p>
     <div class="content">
 
-      <div class="fullWidth">
-         <?php // Start the loop ?>
-        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<!--     1st Div -->    
+    <div class="quarter">
+      <h3 class="beansTitle">Malapascua</h3>
+      <img src="" alt="">
+      <p>Lorem ipsum dolor sit amet.</p>
+      <button class="beansButton">See Menu</button>
+    </div>
+<!--     2nd Div -->    
+    <div class="quarter">
+      <h3 class="beansTitle">Malapascua</h3>
+      <img src="" alt="">
+      <p>Lorem ipsum dolor sit amet.</p>
+      <button class="beansButton">See Menu</button>
+    </div>
+<!--     3rd Div -->    
+    <div class="quarter">
+      <h3 class="beansTitle">Malapascua</h3>
+      <img src="" alt="">
+      <p>Lorem ipsum dolor sit amet.</p>
+      <button class="beansButton">See Menu</button>
+    </div>
+<!--     4th Div -->    
+    <div class="quarter">
+      <h3 class="beansTitle">Malapascua</h3>
+      <img src="" alt="">
+      <p>Lorem ipsum dolor sit amet.</p>
+      <button class="beansButton">See Menu</button>
+    </div>
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-
-      <?php endwhile; // end the loop?>
+    </div> <!--/.content -->
+  
+<!--  middle giant pictures-->    
+  <div class="twins">
+      <div class="leftTwin">
+        <img src="<?php bloginfo('template_directory') ?>/img/coffee_by_bed.jpg" alt="">
       </div>
+<!-- right twin start-->      
+    <div class="rightTwin">
+        <img src="<?php bloginfo('template_directory') ?>/img/Unsplash-Coffee-Cup.jpg" alt="">
+      </div>
+    </div>
 
-     
-    </div> <!-- /,content -->
+<!--    Special menu -->
+    <div class="specialMenu">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, dolorem!</p>
+    </div>
+
+
+        <?php get_template_part( 'loop', 'index' ); ?>
+
+    <?php get_sidebar(); ?>
+
 
     <?php //get_sidebar(); ?>
-
+    <?php //get_template_part( 'loop', 'index' ); ?>
   </div> <!-- /.container -->
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
+
+
+
+
+
+
+<!-- Delete here -->
+
+     
+   
