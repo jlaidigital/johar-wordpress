13b477469a7ab864878557bd8130fb36563640ed
John <lai.john.py@gmail.com>
2015-05-31 15:57:13 -0400

    Add blog page style and removed extra php files

--- page-blog.php
+++ page-blog.php
@@ -2,7 +2,6 @@
 
 /* Template Name:page-blog.php */
 
-
  ?>
 
 
@@ -15,48 +14,36 @@
 
     <div class="content">
 
+        <div class="blogArea">
+          <!--     start of query loop  -->    
+              <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; query_posts(array(
+                'posts_per_page' => 5, // custom post type
+                       'paged'=>$paged,'category_name' => 'Uncategorized'
+                      ));
+              ?>  
+          <!--     Start of loop -->    
+              <?php if(have_posts()) while(have_posts()) : the_post(); ?>
 
+                   <article>
+                        <header class="major">
+                          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        </header>
+                      
+                        <h2><?php single_post_title( 'Current post: ' ); ?></h2>
+                        <span class="image image-full"><?php the_post_thumbnail('full'); ?></span>
 
-    <?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; query_posts(array(
-      'posts_per_page' => 5, // custom post type
-             'paged'=>$paged,
-            ));
-    ?>  
+                        <?php the_excerpt(); ?>
+                      </article>
+              <?php endwhile; ?><!--  end of loop -->
+      </div> <!-- Blog area end -->
 
-    <?php if(have_posts()) while(have_posts()) : the_post(); ?>
-
-
-
-   <article>
-        <header class="major">
-          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        </header>
-      
-        <h2><?php single_post_title( 'Current post: ' ); ?></h2>
-
-
-
-        <span class="image image-full"><?php the_post_thumbnail('full'); ?></span>
-
-        <?php the_excerpt(); ?>
-
-      </article>
-    <?php endwhile; ?>
-
-
+       <div class="sidebarArea">
+        <?php get_sidebar(); ?>
+        <?php //get_template_part( 'loop', 'index' ); ?>
+      </div>
     </div> <!--/.content -->
   
-<!--  middle giant pictures-->    
-
-<!--    Special menu -->
-   
-
-
-    
-
-
-    <?php //get_sidebar(); ?>
-    <?php //get_template_part( 'loop', 'index' ); ?>
+ 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
 
