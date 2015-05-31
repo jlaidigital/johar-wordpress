af36d7f22a1451be6fac6a87b9e7b03842ed56e8
John <lai.john.py@gmail.com>
2015-05-31 17:54:42 -0400

    added contact scss and edited map on contact page

--- page-blog.php
+++ page-blog.php
@@ -12,7 +12,19 @@
 <div class="main">
   <div class="container">
 
-    <div class="content">
+    <div class="content clearfix">
+
+      <div class="blogPage">
+          <?php // Start the loop ?>
+         <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+          <h2><?php the_title(); ?></h2>
+          <?php the_content(); ?>
+
+         <?php endwhile; // end the loop?>
+      </div>
+
+
 
         <div class="blogArea">
           <!--     start of query loop  -->    
